245c3087cd4534c786558aaf9e77cf0252580bc6
Alex Gray <alex@mrgray.com>
2017-02-01 09:08:26 -0500

    consolidated app delegate into main, modularized

--- TagsInputSample/main.m
+++ TagsInputSample/main.m
@@ -1,13 +1,24 @@
-//
-//  main.m
-//  TagsInputSample
-//
-//  Created by Антон Кузнецов on 11.02.15.
-//  Copyright (c) 2015 TheLightPrjg. All rights reserved.
-//
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+/*  main.m
+    TagsInputSample
+
+    Created by Антон Кузнецов on 11.02.15.
+    Copyright (c) 2015 TheLightPrjg. All rights reserved. */
+
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (nonatomic) UIWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)app didFinishLaunchingWithOptions:(NSDictionary *)opts {
+  return YES;
+}
+
+@end
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
